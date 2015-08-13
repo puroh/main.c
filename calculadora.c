f2833a333857a50fd700898c5bc0c21443a8b517
test <diego683@gmail.com>
2015-08-13 12:18:27 -0500

    actualizo calculadora .h y .c

---
+++ calculadora.c
@@ -0,0 +1,24 @@
+#include "calculadora.h"
+
+int suma (int x, int y)
+{
+return x+y;
+
+}
+
+int resta (int x, int y)
+{
+return x-y;
+}
+
+int division (int x, int y)
+{
+return x/y;
+}
+
+int multiplicacion (int x, int y)
+{
+return x*y;
+}
+
+
---
+++ calculadora.h
@@ -0,0 +1,4 @@
+int suma (int x, int y);
+int resta (int x, int y);
+int multiplicacion (int x, int y);
+int division (int x, int y);
