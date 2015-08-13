ac5fe5e8cd5b689db1f30b7c66b8364a8eb7219f
test <diego683@gmail.com>
2015-08-13 12:37:24 -0500

    Se actualizo main.c

--- main.c
+++ main.c
@@ -3,7 +3,29 @@
 
 int main(void)
 {
+int x;
+int y;
+int a;
+int b;
+printf ("que operacion desea hacer?\n 1.suma\n 2.resta\n 3.division\n 4.multiplicacion\");
+scanf ("%d",&a);
+printf (" Ingrese numero 1\n");
+scanf ("%d",&x);
+printf (" Ingrese numero 2\n");
+scanf ("%d",&y);
+switch (a){
 
+	case 1:
+		b=suma(x,y);
+	case 2
+		b=resta(x,y);
+	case 3:
+		b=division(x,y);
+	case 4:
+		b=multiplicacion(x,y);
+}
+
+printf ("El resultado es\n%d",b);
 
 
 return 0;
