bdfd0985f61aa9c932a5138fb74f0ddadd7ea09a
test <diego683@gmail.com>
2015-08-14 20:52:21 -0500

    se han modificaado los documentos por parte del documentador

--- calculadora.c
+++ calculadora.c
@@ -2,23 +2,20 @@
 
 int suma (int x, int y)
 {
-return x+y;
-
+	return x+y;
 }
 
 int resta (int x, int y)
 {
-return x-y;
+	return x-y;
 }
 
 int division (int x, int y)
 {
-return x/y;
+	return x/y;
 }
 
 int multiplicacion (int x, int y)
 {
-return x*y;
+	return x*y;
 }
-
-
--- calculadora.h
+++ calculadora.h
@@ -1,4 +1,28 @@
+/**
+*	\brief Función que Suma
+*	\param x Operador 1
+*	\param y Operador 2
+*	\return Un entero con el Resultado
+*/
 int suma (int x, int y);
+/**
+*	\brief Función que Resta
+*	\param x Operador 1
+*	\param y Operador 2
+*	\return Un entero con el Resultado
+*/
 int resta (int x, int y);
-int multiplicacion (int x, int y);
-int division (int x, int y);
+/**
+*	\brief Función que Multiplica
+*	\param x Operador 1
+*	\param y Operador 2
+*	\return Un entero con el Resultado
+*/
+int multiplicación (int x, int y);
+/**
+*	\brief Función que Divide
+*	\param x Operador 1
+*	\param y Operador 2
+*	\return Un entero con el Resultado
+*/
+int división (int x, int y);
